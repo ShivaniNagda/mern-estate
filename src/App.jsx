542ac23c9c86d6ac650c9cb315ef35ad0094f628
Shivani Nagda <shivaninagda1997@gmail.com>
2024-09-21 21:29:29 +0530

    Create and run server

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,21 @@ import Profile from './pages/profile'
 import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
 import About from './pages/about'
+import Header from './components/header'
 
 export default function App() {
-  return <BrowserRouter>
+  return (
+   <BrowserRouter>
+   <Header />
   <Routes>
-      <Route path='/' element={Home />} />
-      <Route path='/sign-in' element={SignIn />} />
-      <Route path='/sign-up' element={SignUp />} />
-      <Route path='/about' element={About />} />
-      <Route path='/profile' element={Profile />} />
+      <Route path='/' element={<Home/> } />
+      <Route path='/sign-in' element={<SignIn />} />
+      <Route path='/sign-up' element={<SignUp />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/profile' element={<Profile />} />
      
       
   </Routes>
   </BrowserRouter>
+  )
 }
